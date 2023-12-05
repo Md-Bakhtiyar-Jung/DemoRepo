dcfa87eb7f93d09a89412d28c646e9fb8db285c3
Md-Bakhtiyar-Jung <jungbakhtiyar@gmail.com>
2023-12-05 19:15:35 +0530

    main.c added

---
+++ main.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+
+<<<<<<< HEAD
+void quicksort(){
+////
+
+=======
+void insertionsort(){
+     /3/3/3/3/3//3
+   
+>>>>>>> insertionsort
+}
+
+int main(){
+
+  printf("hey there this is bakhtiyar reporting live from faridabad");
+
+}
